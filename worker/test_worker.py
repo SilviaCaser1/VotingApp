07040a44f969ebaae3c509ca2c009fca4143b004
unknown <silvia.casermeiro@gmail.com>
2025-01-19 23:54:28 -0300

    new tests

---
+++ worker/test_worker.py
@@ -0,0 +1,9 @@
+//este es un archivo de prueba 
+import unittest
+
+class TestWorker(unittest.TestCase):
+    def test_example(self):
+        self.assertEqual(1 + 1, 2)
+
+if __name__ == '__main__':
+    unittest.main()
