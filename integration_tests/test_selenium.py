9021301be249751f4b1e4f811ff14affdb7fcbde
unknown <silvia.casermeiro@gmail.com>
2025-01-30 22:24:33 -0300

    test

--- integration_tests/test_selenium.py
+++ integration_tests/test_selenium.py
@@ -12,12 +12,8 @@ def driver():
     chrome_options.add_argument("--no-sandbox")  # Requerido en entornos sin UI
     chrome_options.add_argument("--disable-dev-shm-usage")  # Previene problemas de memoria en contenedores
     chrome_options.add_argument("--disable-gpu")  # Desactiva GPU, útil para headless en Linux
-#    chrome_options.add_argument("--user-data-dir=/tmp/chrome-user-data")  # Usa un directorio único
+    chrome_options.add_argument("--user-data-dir=/tmp/chrome-user-data")  # Usa un directorio único
     chrome_options.add_argument("--remote-debugging-port=9222")  # Evita conflicto con depuración
-
-    # Crear un directorio temporal único para cada ejecución
-    user_data_dir = tempfile.mkdtemp()
-    chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
 
     # Configurar el driver de Chrome
     service = Service(ChromeDriverManager().install())
