24fe9afcf73566d9008b74dbeaa6dd0b9702c486
unknown <silvia.casermeiro@gmail.com>
2025-01-30 22:02:11 -0300

    tests

--- integration_tests/test_selenium.py
+++ integration_tests/test_selenium.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+import tempfile
 
 @pytest.fixture
 def driver():
@@ -11,9 +12,14 @@ def driver():
     chrome_options.add_argument("--no-sandbox")  # Requerido en entornos sin UI
     chrome_options.add_argument("--disable-dev-shm-usage")  # Previene problemas de memoria en contenedores
     chrome_options.add_argument("--disable-gpu")  # Desactiva GPU, útil para headless en Linux
-    chrome_options.add_argument("--user-data-dir=/tmp/chrome-user-data")  # Usa un directorio único
+#    chrome_options.add_argument("--user-data-dir=/tmp/chrome-user-data")  # Usa un directorio único
     chrome_options.add_argument("--remote-debugging-port=9222")  # Evita conflicto con depuración
 
+    # Crear un directorio temporal único para cada ejecución
+    user_data_dir = tempfile.mkdtemp()
+    chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
+
+    # Configurar el driver de Chrome
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
     
